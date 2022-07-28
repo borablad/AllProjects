9e856241a59b1614ee80b8a221a550011c190377
Bora_blad <96587810+borablad@users.noreply.github.com>
2022-07-28 15:03:22 +0600

    Add files via upload

--- sketch_jul28a/sketch_jul28a.ino
+++ sketch_jul28a/sketch_jul28a.ino
@@ -12,8 +12,10 @@ byte green = 0;
 byte blue = 0;
 byte state = 0;
 unsigned int colour = red << 11; // Colour order is RGB 5+6+5 bits each
+
 void pricer(char*title,char*price);
 void pricer_with_promotion(char*title,char*price_promotion,char*price);
+
 void setup(void) {
   Serial.begin(9600);
   tft.init();
@@ -24,7 +26,7 @@ void setup(void) {
   char*prise = "250";
 
   pricer_with_promotion(title,price_Promotion,prise);
-  //pricer(title,prise); // Fill the screen with rainbow colours
+  /pricer(title,prise); 
 
 }
 
@@ -36,16 +38,19 @@ void loop() {
 void pricer_with_promotion(char*title,char*price_promotion,char*price)
 {
   colour = TFT_CYAN ;
-
-  tft.fillRect(190, 190, 200, 200, colour);
+  tft.fillRect(150, 125, 200, 200, colour);
+  colour = TFT_CYAN ;
+  tft.fillRect(180, 60, 150, 150, colour);
+  colour = TFT_WHITE ;
+  tft.fillRect(185, 65, 125, 55, colour);
   
   tft.setTextSize(1);          
   tft.setTextColor(TFT_CYAN );
-  tft.setCursor(15, 18, 2);    
+  tft.setCursor(15, 18, 4);    
   tft.println("Company name");
   tft.setTextSize(1);         
   tft.setTextColor(TFT_BLACK);
-  tft.setCursor(26, 75, 2);   
+  tft.setCursor(3, 75, 4);   
   tft.println(title);
   tft.println(title); 
   tft.setCursor(200, 68, 4);    
@@ -53,30 +58,36 @@ void pricer_with_promotion(char*title,char*price_promotion,char*price)
   tft.setCursor(200, 90, 4);    
   tft.println(price_promotion);
   
-  tft.setCursor(200, 140, 4);    
+  tft.setCursor(175, 135, 4);    
   tft.println("Price:");  
-  tft.setCursor(200, 170, 7);    
+  tft.setCursor(175, 170, 7);    
   tft.println(price);   
 }
 
 void pricer(char*title,char*price)
 {
-   for (int i = 50; i > 0; i--) {
-    colour = TFT_BLACK;
-    tft.drawFastHLine(0, i, tft.width(), colour); 
-   }
-  tft.setTextSize(1);           
-  tft.setTextColor(TFT_WHITE);
-  tft.setCursor(25, 25, 4);   
-  tft.println("Company name");
-  tft.setTextSize(1);           
-  tft.setTextColor(TFT_BLACK);
-  tft.setCursor(36, 75, 4);    
-  tft.println(title);  
-  tft.setCursor(200, 140, 4);   
-  tft.println("Price:"); 
-  tft.setCursor(200, 170, 7);    
-  tft.println(price);  
+  colour = TFT_CYAN ;
+  tft.fillRect(150, 125, 200, 200, colour);
+  colour = TFT_CYAN ;
+  tft.fillRect(180, 60, 150, 150, colour);
+  colour = TFT_WHITE ;
+  tft.fillRect(185, 65, 125, 55, colour);
   
+  tft.setTextSize(1);          
+  tft.setTextColor(TFT_CYAN );
+  tft.setCursor(15, 18, 4);    
+  tft.println("Company name");
+  tft.setTextSize(1);         
+  tft.setTextColor(TFT_BLACK);
+  tft.setCursor(3, 75, 4);   
+  tft.println(title);
+  tft.println(title); 
+
+  
+  tft.setCursor(175, 135, 4);    
+  tft.println("Price:");  
+  tft.setCursor(175, 170, 7);    
+  tft.println(price);   
+}
   
 }
